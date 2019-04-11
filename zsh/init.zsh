a766091ff0699b1c5b86412c1fca886427dcfb09
Zarashi <49112232+zarashi-dev@users.noreply.github.com>
2019-04-12 02:46:14 +0900

    Add git alias

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -2,6 +2,7 @@
 # For Zshell interactive
 # Should be symlinked to ~/.zshrc
 
+# Zplug
 export ZPLUG_HOME="${XDG_DATA_HOME}/zplug"
 export ZPLUG_CACHE_DIR="${XDG_CACHE_HOME}/zplug"
 export ZPLUG_LOADFILE="${XDG_CONFIG_HOME}/zsh/plugins.zsh"
@@ -9,3 +10,8 @@ export ZPLUG_LOADFILE="${XDG_CONFIG_HOME}/zsh/plugins.zsh"
 if [[ -d "${ZPLUG_HOME}" ]]; then
   source "${ZPLUG_HOME}/init.zsh"
 fi
+
+# git
+alias g="git"
+alias gs="git status"
+alias gc="git commit"
