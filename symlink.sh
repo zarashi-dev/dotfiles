4784b905039814157091cdc40d84c560488d0d7d
Zarashi <49112232+zarashi-dev@users.noreply.github.com>
2019-04-15 23:52:05 +0900

    [util] Fix ln to use absolute path

--- symlink.sh
+++ symlink.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 # Utility script to perform neccessary symlinking
 
-export DOTDIR="${0%/*}"
+export DOTDIR="${HOME}/.config"
+
+function ln_if_exists {
+  if [[ -a "$1" ]]; then
+    ln -s $1 $2
+  else
+    echo "$1 is not found" 1>&2
+  fi
+}
 
 # bash
-ln -s "${DOTDIR}/bash/init.bash" "${HOME}/.bash_profile"
-ln -s "${DOTDIR}/bash/init.bash" "${HOME}/.bashrc"
+ln_if_exists "${DOTDIR}/bash/init.bash" "${HOME}/.bash_profile"
+ln_if_exists "${DOTDIR}/bash/init.bash" "${HOME}/.bashrc"
 
 # zsh
-ln -s "${DOTDIR}/zsh/env.zsh" "${HOME}/.zshenv"
-ln -s "${DOTDIR}/zsh/init.zsh" "${HOME}/.zshrc"
+ln_if_exists "${DOTDIR}/zsh/env.zsh" "${HOME}/.zshenv"
+ln_if_exists "${DOTDIR}/zsh/init.zsh" "${HOME}/.zshrc"
